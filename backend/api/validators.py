5cde6f6ce8ea69ff1abd4144477077a99f84b1cb
Viktoriia <a.kostoyanskaya@yandex.ru>
2024-11-18 17:37:09 +0300

    f

--- backend/api/validators.py
+++ backend/api/validators.py
@@ -7,21 +7,3 @@ def validate_tags(value):
     if len(value) != len(set(value)):
         raise serializers.ValidationError("Теги не должны повторяться.")
     return value
-
-
-def validate_ingredients(self, value):
-    if not value:
-        raise serializers.ValidationError("Ингредиент не выбран.")
-
-    ingredient_ids = []
-    for ingredient in value:
-        if ingredient['amount'] < 1:
-            raise serializers.ValidationError(
-                "Количество ингредиента должно быть больше 0"
-            )
-        ingredient_ids.append(ingredient['ingredient']['id'])
-
-    if len(ingredient_ids) != len(set(ingredient_ids)):
-        raise serializers.ValidationError("Ингредиенты не должны повторяться.")
-
-    return value
